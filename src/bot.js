24b551dc3f0c6be37b443f9fa2d49c47dc720946
RostyslavR <rrmail@ukr.net>
2023-04-25 06:00:12 +0300

    upd

---
+++ src/bot.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const shell = require("shelljs");
+
+const { Telegraf } = require("telegraf");
+
+const { BOT_TOKEN } = process.env;
+
+const bot = new Telegraf(BOT_TOKEN);
+
+// bot.start((ctx) => ctx.reply('Welcome'));
+// bot.help((ctx) => ctx.reply('Send me a sticker'));
+// bot.on('sticker', (ctx) => ctx.reply('👍'));
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+// bot.launch();
+
+bot.start((ctx) =>
+  ctx.reply(
+    `Hello, ${ctx.message.from.first_name}! I'm Dan. I can deploy soYummy on Vercel, just type 'deploy'`
+  )
+);
+
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.hears("hello", (ctx) =>
+  ctx.reply(`Hello, ${ctx.message.from.first_name}!`)
+);
+bot.hears("about", (ctx) =>
+  ctx.reply(
+    "I'm a small bot and can deploy soYummy on Vercel yet. ) Type 'deploy' and I will do it."
+  )
+);
+
+bot.hears("deploy", (ctx) => {
+  shell.exec("../../deploy_soyummy.sh");
+  ctx.reply("Done");
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
